d6f196b1d3dd6d9b5317ef2e445735bbef968460
ejonghwan <sun87-1@daum.net>
2024-08-23 23:54:09 +0900

    header 초기작업

--- front/src/components/common/header/Header.tsx
+++ front/src/components/common/header/Header.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import '@/assets/css/common/header.css'
 
 const Header = () => {
   return (
     <header id='header'>
-      <ul className=''>
-        <li></li>
-      </ul>
+      <div className='header-inner'>
+         <h1>logo</h1>
+         <ul className='header-inner-items'>
+            <li>메뉴1</li>
+            <li>메뉴2</li>
+            <li>메뉴3</li>
+            <li>메뉴4</li>
+            <li>메뉴5</li>
+         </ul>
+      </div>
     </header>
   )
 }
