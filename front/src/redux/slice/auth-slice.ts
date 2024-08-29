3cf8b7d14fa9e99f3234cb5a1b4c1cc6a9baa69b
ejonghwan <sun87-1@daum.net>
2024-08-29 23:57:32 +0900

    redux-toolkit 변경

---
+++ front/src/redux/slice/auth-slice.ts
@@ -0,0 +1,11 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {}; // 아직 초기상태 정의되지 않음!
+
+export const auth = createSlice({
+  name: "auth", // slice 이름으로 쓸 문자열!
+  initialState, // 아직 정의되지 않음!
+  reducers: {
+  	// 액션 여기에 추가 예정!!
+  },
+});
---
+++ front/src/redux/store/store.ts
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {
+    //여기에 추후에 리듀서를 넣어둔다.
+  },
+});
