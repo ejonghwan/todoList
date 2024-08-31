c675a1dfceca274c4a8c713d3107b2b1e9c85743
ejonghwan <sun87-1@daum.net>
2024-08-31 20:02:57 +0900

    페이지네이션 버그 해결

--- front/src/components/common/pagenation/Pagenation.tsx
+++ front/src/components/common/pagenation/Pagenation.tsx
@@ -7,7 +7,7 @@ const Pagenations = ({ allLength, pageNum, setPageNum }) => {
     const [pageGroupMinLen, setPageGroupMinLen] = useState(0); //0부터 +-
     const [pageGroupMaxLen, setPageGroupMaxLen] = useState(1);  //1부터 +-
     const pageGroupNumberRef = useRef(9); // 보여지는 아이템 갯수 .
-    const test = useRef(Math.ceil(11 / 5) ); // 보여지는 페이징 숫자.
+    const test = useRef(5); // 보여지는 페이징 숫자.
 
     const handlePrevPageGroup = () => {
         setPageGroupMinLen(prev => prev - 1);
@@ -31,14 +31,14 @@ const Pagenations = ({ allLength, pageNum, setPageNum }) => {
                     disabled={pageGroupMinLen === 0 && true}
                     className={'button_reset button_type4 button_type_arrow_l hover_type1'}
                 >
-                    <span className='blind'>이전 {pageGroupNumberRef.current} 페이지</span>
+                    <span className='blind'>이전 {test.current} 페이지</span>
                 </Button>
             </div>
 
             {Array(Math.ceil(allLength / pageGroupNumberRef.current)).fill(null).map((_, idx) => {
                 return (
                     <Fragment key={idx}>
-                        {idx >= (pageGroupMinLen * pageGroupNumberRef.current) &&  idx < (pageGroupMaxLen * pageGroupNumberRef.current) && (
+                        {idx >= (pageGroupMinLen * test.current) &&  idx < (pageGroupMaxLen * test.current) && (
                             <Button 
                                 className={`button_reset page_number_item hover_type1 ${pageNum === idx + 1 ? 'active' : ''}`}
                                 onClick={() => setPageNum(idx + 1)}
@@ -51,7 +51,7 @@ const Pagenations = ({ allLength, pageNum, setPageNum }) => {
             <div className='next_btn_wrap' style={{ border: '1px solid red' }}>
                 <Button 
                     onClick={handleNextPageGroup} 
-                    disabled={allLength / pageGroupNumberRef.current <= pageGroupMaxLen * pageGroupNumberRef.current && true} 
+                    disabled={allLength / pageGroupNumberRef.current <= pageGroupMaxLen * test.current && true} 
                     // 검색갯수 / 보여줄페이징 <= 페이징맥스넘버 * 보여줄페이징
                     className={'button_reset button_type4 button_type_arrow_r hover_type1'}
                 >
@@ -62,18 +62,40 @@ const Pagenations = ({ allLength, pageNum, setPageNum }) => {
 
 
 
-            {Array( Math.ceil(allLength / pageGroupNumberRef.current) ).fill(null).map((_, idx) => {
-                return (
-                    <Fragment key={idx}>
-                        {idx >= (pageGroupMinLen * pageGroupNumberRef.current / 2) && idx < (pageGroupMaxLen * pageGroupNumberRef.current / 2) && (
-                            <Button 
-                                className={`button_reset page_number_item hover_type1 ${pageNum === idx + 1 ? 'active' : ''}`}
-                                onClick={() => setPageNum(idx + 1)}
-                            >{idx + 1}</Button>
-                        )}
-                    </Fragment>
-                );
-            })}
+<div className='prev_btn_wrap' style={{ border: '1px solid red' }}>
+    <Button 
+        onClick={handlePrevPageGroup}
+        disabled={pageGroupMinLen === 0 && true}
+        className={'button_reset button_type4 button_type_arrow_l hover_type1'}
+    >
+        <span className='blind'>이전 {pageGroupNumberRef.current} 페이지</span>
+    </Button>
+</div>
+
+
+{Array( Math.ceil(allLength / pageGroupNumberRef.current) ).fill(null).map((_, idx) => {
+    return (
+        <Fragment key={idx}>
+            {idx >= (pageGroupMinLen * pageGroupNumberRef.current) && idx < (pageGroupMaxLen * pageGroupNumberRef.current) && (
+                <Button 
+                    className={`button_reset page_number_item hover_type1 ${pageNum === idx + 1 ? 'active' : ''}`}
+                    onClick={() => setPageNum(idx + 1)}
+                >{idx + 1}</Button>
+            )}
+        </Fragment>
+    );
+})}
+
+<div className='next_btn_wrap' style={{ border: '1px solid red' }}>
+    <Button 
+        onClick={handleNextPageGroup} 
+        disabled={allLength / pageGroupNumberRef.current <= pageGroupMaxLen * pageGroupNumberRef.current && true} 
+        // 검색갯수 / 보여줄페이징 <= 페이징맥스넘버 * 보여줄페이징
+        className={'button_reset button_type4 button_type_arrow_r hover_type1'}
+    >
+        <span className='blind'>다음 {pageGroupNumberRef.current} 페이지</span>
+    </Button>
+</div>
 
 
 
