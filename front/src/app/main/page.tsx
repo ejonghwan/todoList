60b99f9fa5cf998cacf710b5322ca112c2dfdb99
ejonghwan <sun87-1@daum.net>
2024-10-26 19:18:16 +0900

    asd

--- front/src/app/main/page.tsx
+++ front/src/app/main/page.tsx
@@ -97,7 +97,7 @@ const MainPage = () => {
                                 )
                             })}
                         </div>
-
+asd
 
                         
                         {/* test 2 */}
